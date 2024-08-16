f24971741679b2983678f392e01e5fff17d69427
Khafid224 <abdulhafid1230@gmail.com>
2024-08-16 20:11:04 +0700

    Update Data ke Dalam Database

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@
 import { Hono } from 'hono';
 
 //import controller
-import { getPosts, createPost, getPostById } from '../controllers/PostController';
+import { getPosts, createPost, getPostById, updatePost } from '../controllers/PostController';
 
 //inistialize router
 const router = new Hono()
@@ -15,5 +15,8 @@ router.post('/', (c) => createPost(c));
 
 //routes posts detail
 router.get('/:id', (c) => getPostById(c));
+
+//route post update
+router.patch('/:id', (c) => updatePost(c));
 
 export const Routes = router;
