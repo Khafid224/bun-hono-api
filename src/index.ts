7f68ea61440efda2e0945f5af7a18a583c25c1f9
Khafid224 <abdulhafid1230@gmail.com>
2024-08-16 20:00:31 +0700

    Menampilkan List Data

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+//import hono
 import { Hono } from 'hono'
 
-const app = new Hono()
+//import routes
+import { Routes } from './routes'
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// Initialize the Hono app
+const app = new Hono().basePath('/api')
+
+// Posts Routes
+app.route('/posts', Routes)
 
 export default app
